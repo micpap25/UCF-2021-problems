57da81f06bb66557bd9d8546f1f709c73893b78c
Michael Papas <micpap25@gmail.com>
2021-03-20 11:24:13 -0400

    template and practice problems

---
+++ src/template.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class template{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out, true);
+        br.readLine();
+        String str;
+        while ((str = br.readLine()) != null) {
+
+        }
+    }
+}
