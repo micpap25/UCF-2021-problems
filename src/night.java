eeba8a76f203cbc36961a7e14020986f5c5b5e1a
Yash Choksey <yascho21@bergen.org>
2021-03-20 15:53:07 -0400

    Night

---
+++ src/night.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class night {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out, true);
+        br.readLine();
+        String str;
+        while ((str = br.readLine()) != null) {
+
+        }
+    }
+}
