3fe088446f0790cdef4ee336ce43f103ede3edf8
Michael Papas <micpap25@gmail.com>
2021-03-20 16:04:50 -0400

    Update expectations.java

--- src/expectations.java
+++ src/expectations.java
@@ -6,15 +6,15 @@ public class expectations{
         PrintWriter pw = new PrintWriter(System.out, true);
         
         
-        int numlines= Integer.parseInt(br.readLine());
+        long numlines= Long.parseLong(br.readLine());
         String str;
         for ( int j=0;j<numlines;j++) {
             str = br.readLine();
             String[] vals = str.split(" ");
-            int curScore = Integer.parseInt(vals[1]);
-            int makeUpPoints = 0;
+            long curScore = Long.parseLong(vals[1]);
+            long makeUpPoints = 0;
             for (int i = 2; i < vals.length; i++){
-                int score = Integer.parseInt(vals[i]);
+                long score = Long.parseLong(vals[i]);
                 if (score <= curScore){
                     makeUpPoints += curScore - score + 1;
                     curScore = curScore + 1;
